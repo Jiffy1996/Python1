c934717d4af3c809b4b561bb29f90dfe1c724060
ivan.korotkevich <kor-ivan11@yandex.ru>
2022-08-10 15:48:23 +0300

    1.1

--- function.py
+++ function.py
@@ -1,2 +1,17 @@
 def hellow():
     print('hellow')
+
+def fileopen():
+    print ('Открываем на чтение файл и по необходимости выводим в другой файл или консоль')
+    Fin = open('C:/python/input.txt')
+    choose = input('Выводим в файл?')
+    if choose == 'да':
+        Fout = open('C:/python/output.txt','w')
+        for str in Fin:
+            Fout.write(str + "\n")
+        print('вывели')
+    else:
+        for str in Fin:
+            mass = Fin.read()
+            smass = mass.split()
+            print(mass, end="")
--- main.py
+++ main.py
@@ -1,26 +1,4 @@
-# This is a sample Python script.
-
 import function
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+function.fileopen()
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-y = int(input('веди значение:'))
-x = int(input('еще одно значение:'))
-
-if __name__ == '__main__':
-    print_hi('PyCharm')
-    function.hellow()
-
-
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
